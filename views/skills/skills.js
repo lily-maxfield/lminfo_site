27d0840fd4b9d37e5c1a715ce801e3f390ccd448
Lily Maxfield <lilymaxfield@outlook.com>
2024-04-08 16:06:16 +0100

    LM - First test of dynamically adding skills to page body

--- views/skills/skills.js
+++ views/skills/skills.js
@@ -1,10 +1,19 @@
-var obj;
+var skills;
 
-fetch('skills.js')
+fetch('skills.json')
     .then(res => res.json())
     .then(data => {
-        obj = data;
+        skills = data;
     })
     .then(() => {
         console.log(obj);
     });
+
+const skillsDiv = document.getElementById("skills-div");
+
+for (skill in skills) {
+    const divRef = document.createElement("div");
+    divRef.setAttribute("name", skill);
+    skillsDiv.appendChild(divRef);
+    
+}
