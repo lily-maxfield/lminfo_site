01f243f04db25138a30838df4c04519f35973c44
Lily Maxfield <lilymaxfield@outlook.com>
2024-04-09 14:32:26 +0100

    LM - Added diagnostic console logs for searchVal and textDiv

--- views/skills/filterSkills.js
+++ views/skills/filterSkills.js
@@ -1,15 +1,15 @@
 function filter() {
     var searchBar = document.getElementById("skills-search");
     var searchVal = searchBar.value;
+    console.log(searchVal);
     var skillsDiv = document.getElementById("skills-list");
 
     var skillsDivs = skillsDiv.children;
     console.log(skillsDivs);
 
     for (skill of skillsDivs) {
-        console.log(skill);
         var textDiv = skill.querySelector('.skill-text');
-        console.log(textDiv);
+        console.log(textDiv.innerHTML);
         if (searchVal.includes(textDiv.innerHTML)) {
             skill.style.visibility = 'visible';
         }
