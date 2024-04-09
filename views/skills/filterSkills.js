5a5d45c94807f1b0cb43c86f754bd973541285c2
Lily Maxfield <lilymaxfield@outlook.com>
2024-04-09 15:58:08 +0100

    LM - Fixed skills filter function

--- views/skills/filterSkills.js
+++ views/skills/filterSkills.js
@@ -1,20 +1,16 @@
 function filter() {
     var searchBar = document.getElementById("skills-search");
-    var searchVal = searchBar.value;
-    console.log(searchVal);
+    var searchVal = searchBar.value.toLowerCase();
     var skillsDiv = document.getElementById("skills-list");
 
     var skillsDivs = skillsDiv.children;
-    console.log(skillsDivs);
 
     for (skill of skillsDivs) {
-        var textDiv = skill.querySelector('.skill-text');
-        console.log(textDiv.innerHTML);
-        if (searchVal.includes(textDiv.innerHTML)) {
-            skill.style.visibility = 'visible';
-        }
-        else {
-            skill.style.visibility = 'hidden';
+        var skillText = skill.querySelector('.skill-text').innerHTML.toLowerCase();
+        if (skillText.includes(searchVal)) {
+            skill.style.display = 'block';
+        } else {
+            skill.style.display = 'none';
         }
     }
 }
