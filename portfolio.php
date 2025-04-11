96ae89bbf685f16465297d1ee3aabe34c3c17101
Lily Maxfield <lilymaxfield@outlook.com>
2025-04-11 11:21:50 +0100

    Starting to add links for portfolio items

--- portfolio.php
+++ portfolio.php
@@ -11,7 +11,65 @@
         <div class="content-wrap">
             <h1 class="lminfo-h1">Portfolio</h1>
 
-            <div class="spacer-div-single"></div>
+            <p class="lminfo-p">Welcome to my portfolio! I hope to share some of my recent projects here - please reach out to me if you have any questions or feedback.</p>
+
+            <div class="lminfo-callout-container">
+                <div class="lminfo-callout">
+                    <!--Website-->
+                    <div>
+                        <div class="lminfo-callout-title">
+                            <p>Portfolio</p>
+                        </div>
+                    </div>
+                    <div class="lminfo-callout-details">
+                        <p class="lminfo-p">Personal website showcasing experience and skills.</p>
+                    </div>
+                </div>
+                <div class="lminfo-callout">
+                    <!--Sat tracker-->
+                    <div>
+                        <div class="lminfo-callout-title">
+                            <p>Satellite Tracker</p>
+                        </div>
+                    </div>
+                    <div class="lminfo-callout-details">
+                        <p class="lminfo-p">.NET program for tracking satellites from live data.</p>
+                    </div>
+                </div>
+                <div class="lminfo-callout">
+                    <!--Midi-->
+                    <div>
+                        <div class="lminfo-callout-title">
+                            <p>MIDI Generator</p>
+                        </div>
+                    </div>
+                    <div class="lminfo-callout-details">
+                        <p class="lminfo-p">Python program for generating music through Windows in-built winmm.dll.</p>
+                    </div>
+                </div>
+                <div class="lminfo-callout">
+                    <!--WAD explorer-->
+                    <div>
+                        <div class="lminfo-callout-title">
+                            <p>DOOM WAD Explorer</p>
+                        </div>
+                    </div>
+                    <div class="lminfo-callout-details">
+                        <p class="lminfo-p">.NET program for exploring content inside of DOOM (1993) WAD files.</p>
+                    </div>
+                </div>
+                <div class="lminfo-callout">
+                    <!--Open Protocol conversion-->
+                    <div>
+                        <div class="lminfo-callout-title">
+                            <p>Open Protocol Driver</p>
+                        </div>
+                    </div>
+                    <div class="lminfo-callout-details">
+                        <p class="lminfo-p">.NET library for reading/writing to Atlas Copco tools using Open Protocol.</p>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
     <?php include 'footer.php';?>
